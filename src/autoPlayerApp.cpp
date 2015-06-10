71af387cb261efbe4ccad646835de3df0585ca83
toddberreth <todd@studioberreth.io>
2015-06-10 13:39:30 -0400

    update commit

--- src/autoPlayerApp.cpp
+++ src/autoPlayerApp.cpp
@@ -25,7 +25,7 @@ void autoPlayerApp::setup(){
 //--------------------------------------------------------------
 void autoPlayerApp::exit(){
     
-    if ((int)events.size() > 0){ for ( int i = 0; i < events.size(); i++ ) delete events[i]; events.clear();}
+    if ((int)events.size() > 0){ for ( int i = 0; i < events.size(); i++ ) delete events[i];}
 }
 
 //--------------------------------------------------------------
@@ -78,10 +78,7 @@ void autoPlayerApp::keyPressed(int key){
     if (key == 's') data->bShowData = !data->bShowData;
 }
 
-//--------------------------------------------------------------
-void autoPlayerApp::keyReleased(int key){
 
-}
 
 //--------------------------------------------------------------
 void autoPlayerApp::mouseMoved(int x, int y ){
@@ -110,6 +107,7 @@ void autoPlayerApp::countSounds(){
     }
 }
 
+//--------------------------------------------------------------
 void autoPlayerApp::loadConfig(){
     
     bool bLoaded = false;
@@ -146,7 +144,6 @@ void autoPlayerApp::loadConfig(){
         tempString = ofToLower(data->config.getValue("GLOBAL:INPUT_TYPE", "off"));
         if (tempString == "mouse") data->inputType = MOUSE; else if (tempString == "cameras") data->inputType = CAMERAS; else if (tempString == "off") data->inputType = OFF;
         
-        //cout << numTimedEvents << endl;
         //TIMED EVENTS
         data->config.pushTag("TIMED_EVENTS", 0);
         int numTimedEvents = data->config.getNumTags("EVENT");
@@ -180,13 +177,12 @@ void autoPlayerApp::loadConfig(){
         }
         data->config.popTag();
         
-        
+        //sorts events by layer
         sort(events.begin(),events.end(), autoPlayerMedia::sortGreaterLayer);
     }
 }
 
-
-
+//--------------------------------------------------------------
 void autoPlayerApp::loadEvent(autoPlayerMedia *newEvent, int i){
     
     string tempString;
@@ -244,13 +240,14 @@ void autoPlayerApp::loadEvent(autoPlayerMedia *newEvent, int i){
     newEvent->playDuration = data->config.getValue("EVENT:DURATION", 0.0, i);
     tempString = ofToLower(data->config.getValue("EVENT:REPEATABLE", "TRUE", i));
     if (tempString == "true") newEvent->bRepeatable = true; else newEvent->bRepeatable = false;
-    
     newEvent->trigger_x_min = data->config.getValue("EVENT:TRIGGER:X:MIN", 0.0, i);
     newEvent->trigger_x_max = data->config.getValue("EVENT:TRIGGER:X:MAX", 0.0, i);
     newEvent->trigger_y_min = data->config.getValue("EVENT:TRIGGER:Y:MIN", 0.0, i);
     newEvent->trigger_y_max = data->config.getValue("EVENT:TRIGGER:Y:MAX", 0.0, i);
 };
 
+//--------------------------------------------------------------
+void autoPlayerApp::keyReleased(int key){}
 //--------------------------------------------------------------
 void autoPlayerApp::mouseDragged(int x, int y, int button){}
 //--------------------------------------------------------------
@@ -267,156 +264,3 @@ void autoPlayerApp::windowResized(int w, int h){}
 void autoPlayerApp::gotMessage(ofMessage msg){}
 //--------------------------------------------------------------
 void autoPlayerApp::dragEvent(ofDragInfo dragInfo){}
-
-
-/*
- data->message = "loading config.xml";
- if( data->config.LoadFile(ofFilePath::getAbsolutePath(DEFAULT_CONFIG, TRUE).c_str()) ){data->message = "config.xml loaded!";}
- else{data->message = "unable to load config.xml check data/ folder";}
- 
- TiXmlNode*		node;
- TiXmlElement*	element;
- TiXmlNode*		eventNode;
- TiXmlElement*	eventElement;
- 
- for(TiXmlNode* node = data->config.FirstChild(); node; node = node->NextSibling() ){
- 
- for( element = node->FirstChildElement(); element; element = element->NextSiblingElement() ){
- 
- 
- 
- if (node->ValueStr()=="TimedEvent") {
- 
- if (element->ValueStr()=="Timed") {
- autoPlayerMedia * newTimedEvent = new autoPlayerMedia(TIMED_EVENT, data);
- string tempString;
- element->QueryStringAttribute("start", &tempString); tempString = ofToLower(tempString);
- if (tempString == "start") newTimedEvent->startTime = 0;
- else {element->QueryFloatAttribute("start", &newTimedEvent->startTime);}
- element->QueryStringAttribute("end", &tempString); tempString = ofToLower(tempString);
- if (tempString == "end") newTimedEvent->endTime = FLT_MAX;
- else {element->QueryFloatAttribute("end", & newTimedEvent->endTime);}
- element->QueryStringAttribute("type", &tempString); tempString = ofToLower(tempString);
- if (tempString == "image") newTimedEvent->mediaType = AP_IMAGE; if (tempString == "video") newTimedEvent->mediaType = AP_VIDEO; if (tempString == "sound") newTimedEvent->mediaType = AP_SOUND;
- if (tempString == "random_image") newTimedEvent->mediaType = AP_RANDOM_IMAGE; if (tempString == "random_video") newTimedEvent->mediaType = AP_RANDOM_VIDEO; if (tempString == "random_sound") newTimedEvent->mediaType = AP_RANDOM_SOUND;
- if (tempString == "image_sequence") newTimedEvent->mediaType = AP_IMAGE_SEQUENCE; if (tempString == "video_sequence") newTimedEvent->mediaType = AP_VIDEO_SEQUENCE; if (tempString == "sound_sequence") newTimedEvent->mediaType = AP_SOUND_SEQUENCE;
- element->QueryStringAttribute("location", &tempString); tempString = ofToLower(tempString);
- if (tempString == "full") newTimedEvent->location = FULL; if (tempString == "l_half") newTimedEvent->location = L_HALF; if (tempString == "r_half") newTimedEvent->location = R_HALF;
- if (tempString == "tile") newTimedEvent->location = TILE; if (tempString == "random_tile") newTimedEvent->location = RANDOM_TILE; if (tempString == "random") newTimedEvent->location = RANDOM_ALL;
- element->QueryIntAttribute("tile_h", &newTimedEvent->tile_h);
- element->QueryIntAttribute("tile_v", &newTimedEvent->tile_v);
- element->QueryStringAttribute("path", &newTimedEvent->mediaPath);
- element->QueryFloatAttribute("alpha", &newTimedEvent->alphaOrig);
- element->QueryStringAttribute("alpha_transition", &tempString); tempString = ofToLower(tempString);
- if (tempString == "true") {newTimedEvent->bAlphaTransition = true;} else {newTimedEvent->bAlphaTransition = false;}
- element->QueryFloatAttribute("alpha_transition_time", &newTimedEvent->alphaTransitionTime);
- element->QueryFloatAttribute("volume", &newTimedEvent->volumeOrig);
- element->QueryStringAttribute("volume_transition", &tempString); tempString = ofToLower(tempString);
- if (tempString == "true") {newTimedEvent->bVolumeTransition = true;} else {newTimedEvent->bVolumeTransition = false;}
- element->QueryFloatAttribute("volume_transition_time", &newTimedEvent->volumeTransitionTime);
- element->QueryStringAttribute("loop", &tempString); tempString = ofToLower(tempString);
- if (tempString == "true") {newTimedEvent->mediaLoop = true;} else {newTimedEvent->mediaLoop = false;}
- element->QueryFloatAttribute("speed", &newTimedEvent->speed);
- element->QueryIntAttribute("layer", &newTimedEvent->layer);
- newTimedEvent->setup();
- events.push_back(newTimedEvent);
- }
- }
- 
- 
- if (node->ValueStr()=="RandomEvent") {
- 
- if (element->ValueStr()=="Random") {
- autoPlayerMedia * newRandomEvent = new autoPlayerMedia(RANDOM_EVENT, data);
- string tempString;
- element->QueryStringAttribute("start", &tempString); tempString = ofToLower(tempString);
- if (tempString == "start") newRandomEvent->startTime = 0;
- else {element->QueryFloatAttribute("start", &newRandomEvent->startTime);}
- element->QueryStringAttribute("end", &tempString); tempString = ofToLower(tempString);
- if (tempString == "end") newRandomEvent->endTime = FLT_MAX;
- else {element->QueryFloatAttribute("end", & newRandomEvent->endTime);}
- element->QueryStringAttribute("type", &tempString); tempString = ofToLower(tempString);
- if (tempString == "image") newRandomEvent->mediaType = AP_IMAGE; if (tempString == "video") newRandomEvent->mediaType = AP_VIDEO; if (tempString == "sound") newRandomEvent->mediaType = AP_SOUND;
- if (tempString == "random_image") newRandomEvent->mediaType = AP_RANDOM_IMAGE; if (tempString == "random_video") newRandomEvent->mediaType = AP_RANDOM_VIDEO; if (tempString == "random_sound") newRandomEvent->mediaType = AP_RANDOM_SOUND;
- if (tempString == "image_sequence") newRandomEvent->mediaType = AP_IMAGE_SEQUENCE; if (tempString == "video_sequence") newRandomEvent->mediaType = AP_VIDEO_SEQUENCE; if (tempString == "sound_sequence") newRandomEvent->mediaType = AP_SOUND_SEQUENCE;
- element->QueryStringAttribute("location", &tempString); tempString = ofToLower(tempString);
- if (tempString == "full") newRandomEvent->location = FULL; if (tempString == "l_half") newRandomEvent->location = L_HALF; if (tempString == "r_half") newRandomEvent->location = R_HALF;
- if (tempString == "tile") newRandomEvent->location = TILE; if (tempString == "random_tile") newRandomEvent->location = RANDOM_TILE; if (tempString == "random") newRandomEvent->location = RANDOM_ALL;
- element->QueryIntAttribute("tile_h", &newRandomEvent->tile_h);
- element->QueryIntAttribute("tile_v", &newRandomEvent->tile_v);
- element->QueryStringAttribute("path", &newRandomEvent->mediaPath);
- element->QueryFloatAttribute("alpha", &newRandomEvent->alphaOrig);
- element->QueryStringAttribute("alpha_transition", &tempString); tempString = ofToLower(tempString);
- if (tempString == "true") {newRandomEvent->bAlphaTransition = true;} else {newRandomEvent->bAlphaTransition = false;}
- element->QueryFloatAttribute("alpha_transition_time", &newRandomEvent->alphaTransitionTime);
- element->QueryFloatAttribute("volume", &newRandomEvent->volumeOrig);
- element->QueryStringAttribute("volume_transition", &tempString); tempString = ofToLower(tempString);
- if (tempString == "true") {newRandomEvent->bVolumeTransition = true;} else {newRandomEvent->bVolumeTransition = false;}
- element->QueryFloatAttribute("volume_transition_time", &newRandomEvent->volumeTransitionTime);
- element->QueryStringAttribute("loop", &tempString); tempString = ofToLower(tempString);
- if (tempString == "true") {newRandomEvent->mediaLoop = true;} else {newRandomEvent->mediaLoop = false;}
- element->QueryFloatAttribute("speed", &newRandomEvent->speed);
- element->QueryIntAttribute("layer", &newRandomEvent->layer);
- element->QueryFloatAttribute("chance", &newRandomEvent->chance);
- element->QueryFloatAttribute("duration", &newRandomEvent->playDuration);
- element->QueryStringAttribute("repeatable", &tempString); tempString = ofToLower(tempString);
- if (tempString == "true") {newRandomEvent->bRepeatable = true;} else {newRandomEvent->bRepeatable = false;}
- 
- newRandomEvent->setup();
- events.push_back(newRandomEvent);
- }
- 
- }
- 
- 
- if (node->ValueStr()=="TriggeredEvent") {
- 
- if (element->ValueStr()=="Trigger") {
- autoPlayerMedia * newTriggeredEvent = new autoPlayerMedia(TRIGGERED_EVENT, data);
- string tempString;
- element->QueryStringAttribute("start", &tempString); tempString = ofToLower(tempString);
- if (tempString == "start") newTriggeredEvent->startTime = 0;
- else {element->QueryFloatAttribute("start", &newTriggeredEvent->startTime);}
- element->QueryStringAttribute("end", &tempString); tempString = ofToLower(tempString);
- if (tempString == "end") newTriggeredEvent->endTime = FLT_MAX;
- else {element->QueryFloatAttribute("end", & newTriggeredEvent->endTime);}
- element->QueryStringAttribute("type", &tempString); tempString = ofToLower(tempString);
- if (tempString == "image") newTriggeredEvent->mediaType = AP_IMAGE; if (tempString == "video") newTriggeredEvent->mediaType = AP_VIDEO; if (tempString == "sound") newTriggeredEvent->mediaType = AP_SOUND;
- if (tempString == "random_image") newTriggeredEvent->mediaType = AP_RANDOM_IMAGE; if (tempString == "random_video") newTriggeredEvent->mediaType = AP_RANDOM_VIDEO; if (tempString == "random_sound") newTriggeredEvent->mediaType = AP_RANDOM_SOUND;
- if (tempString == "image_sequence") newTriggeredEvent->mediaType = AP_IMAGE_SEQUENCE; if (tempString == "video_sequence") newTriggeredEvent->mediaType = AP_VIDEO_SEQUENCE; if (tempString == "sound_sequence") newTriggeredEvent->mediaType = AP_SOUND_SEQUENCE;
- element->QueryStringAttribute("location", &tempString); tempString = ofToLower(tempString);
- if (tempString == "full") newTriggeredEvent->location = FULL; if (tempString == "l_half") newTriggeredEvent->location = L_HALF; if (tempString == "r_half") newTriggeredEvent->location = R_HALF;
- if (tempString == "tile") newTriggeredEvent->location = TILE; if (tempString == "random_tile") newTriggeredEvent->location = RANDOM_TILE; if (tempString == "random") newTriggeredEvent->location = RANDOM_ALL;
- element->QueryIntAttribute("tile_h", &newTriggeredEvent->tile_h);
- element->QueryIntAttribute("tile_v", &newTriggeredEvent->tile_v);
- element->QueryStringAttribute("path", &newTriggeredEvent->mediaPath);
- element->QueryFloatAttribute("alpha", &newTriggeredEvent->alphaOrig);
- element->QueryStringAttribute("alpha_transition", &tempString); tempString = ofToLower(tempString);
- if (tempString == "true") {newTriggeredEvent->bAlphaTransition = true;} else {newTriggeredEvent->bAlphaTransition = false;}
- element->QueryFloatAttribute("alpha_transition_time", &newTriggeredEvent->alphaTransitionTime);
- element->QueryFloatAttribute("volume", &newTriggeredEvent->volumeOrig);
- element->QueryStringAttribute("volume_transition", &tempString); tempString = ofToLower(tempString);
- if (tempString == "true") {newTriggeredEvent->bVolumeTransition = true;} else {newTriggeredEvent->bVolumeTransition = false;}
- element->QueryFloatAttribute("volume_transition_time", &newTriggeredEvent->volumeTransitionTime);
- element->QueryStringAttribute("loop", &tempString); tempString = ofToLower(tempString);
- if (tempString == "true") {newTriggeredEvent->mediaLoop = true;} else {newTriggeredEvent->mediaLoop = false;}
- element->QueryFloatAttribute("speed", &newTriggeredEvent->speed);
- element->QueryIntAttribute("layer", &newTriggeredEvent->layer);
- element->QueryFloatAttribute("chance", &newTriggeredEvent->chance);
- element->QueryFloatAttribute("duration", &newTriggeredEvent->playDuration);
- element->QueryStringAttribute("repeatable", &tempString); tempString = ofToLower(tempString);
- if (tempString == "true") {newTriggeredEvent->bRepeatable = true;} else {newTriggeredEvent->bRepeatable = false;}
- 
- element->QueryFloatAttribute("trigger_x_min", &newTriggeredEvent->trigger_x_min);
- element->QueryFloatAttribute("trigger_x_max", &newTriggeredEvent->trigger_x_max);
- element->QueryFloatAttribute("trigger_y_min", &newTriggeredEvent->trigger_y_min);
- element->QueryFloatAttribute("trigger_y_max", &newTriggeredEvent->trigger_y_max);
- 
- newTriggeredEvent->setup();
- events.push_back(newTriggeredEvent);
- }
- }
- 
- }
- }
- */
